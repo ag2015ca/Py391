5e70b9b04d832460fbabf26e2839e552fc853931
anu2015ca <anu2015ca@gmail.com>
2021-03-12 19:34:57 -0800

    adding thirdfunction

--- function1.py
+++ function1.py
@@ -2,5 +2,10 @@ def firstfunction():
     print('hello')
     
 def secondfunction():
-    print('hello second')   
+    print('hello second')
+def thirdfunction():
+    print('hello second')  
+
+firstfunction()
 secondfunction()
+thirdfunction()
